2caa1e02d244de6b90742a94b017329c00902089
Zach Parker <parkerz410@gmail.com>
2019-05-17 18:00:39 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,10 +22,9 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  while counter <= number_of_times
-    counter += 1
+  while counter < number_of_times
     puts phrase
-
+    counter += 1
   end
 end
 
